67c1367351242575b83b92a550c62f123b822098
Sakari Cajanus <scajanus@gmail.com>
2020-01-13 16:22:50 +0200

    Add heroku-specific configs

--- mbse/settings.py
+++ mbse/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'vsrjw0@gdhp14l&mzg-=yv$3u0l&f_my^o7bn_5_*v4q_mhl^('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ['moonboardsearchengine.herokuapp.com']
 ALLOWED_HOSTS = []
 
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,3 +120,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, "static")
+            ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
--- mbse/wsgi.py
+++ mbse/wsgi.py
@@ -6,11 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
-
 import os
 
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mbse.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my-application.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
