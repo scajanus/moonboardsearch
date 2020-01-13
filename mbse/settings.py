1d5820fa2e36a34727f1e3b4604202816f707ae8
Sakari Cajanus <scajanus@gmail.com>
2020-01-13 16:43:59 +0200

    Correct settings for whitenoise

--- mbse/settings.py
+++ mbse/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
             os.path.join(BASE_DIR, "static")
             ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
