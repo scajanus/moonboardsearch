3efede4d5f1886b7f908441c1d85197feac59d02
Sakari Cajanus <scajanus@gmail.com>
2020-01-13 16:50:15 +0200

    revert wsgi config

--- mbse/wsgi.py
+++ mbse/wsgi.py
@@ -6,12 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
+
 import os
 
-from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my-application.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mbse.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
